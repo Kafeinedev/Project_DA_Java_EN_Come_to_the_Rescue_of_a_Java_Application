030fc44219e43e2aeeab9d6a3938855d678ede03
Kafeinedev <kafeinedev@protonmail.com>
2021-07-30 09:39:39 +0200

    Storing symptoms data into a TreeMap. Results are now complete and alphabeticaly ordered.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,36 +4,33 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.TreeMap;
+import java.util.Map;
 
 public class AnalyticsCounter {
 
+	static TreeMap<String, Integer> data = new TreeMap<String, Integer>();
+
 	public static void main(String args[]) throws Exception {
-		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int headCount = 0;
-		int rashCount = 0;
-		int dilatedPupilCount = 0;
-
 		try {
-			while (line != null) {
-				System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headCount++;
-				} else if (line.equals("rash")) {
-					rashCount++;
-				} else if (line.equals("dialated pupils")) {
-					dilatedPupilCount++;
-				}
+			BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+			String line = reader.readLine();
 
+			while (line != null) {
+				Integer count = data.get(line);
+				if (count != null) {
+					data.put(line, count + 1);
+				} else {
+					data.put(line, 1);
+				}
 				line = reader.readLine();
 			}
 			reader.close();
 
 			FileWriter writer = new FileWriter("result.out");
-			writer.write("headache: " + headCount + "\n");
-			writer.write("rash: " + rashCount + "\n");
-			writer.write("dialated pupils: " + dilatedPupilCount + "\n");
+			for (Map.Entry<String, Integer> traverse : data.entrySet()) {
+				writer.write(traverse.getKey() + ' ' + traverse.getValue() + '\n');
+			}
 			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
